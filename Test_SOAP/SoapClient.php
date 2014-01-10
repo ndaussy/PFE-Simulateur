8c770bdc078de49a00630820210e273cb70e1cc7
ndaussy <ndaussy@gmail.com>
2014-01-10 19:21:46 +0100

    Modification du service Test :

--- Test_SOAP/SoapClient.php
+++ Test_SOAP/SoapClient.php
@@ -1,20 +1,19 @@
 <?php
-/**
- * Invocation du Service WEB que nous venons de créer
- */
-//include('./SoapClientPort.php');
+try
+{
+$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+socket_connect($sock,"239.255.42.21", 10000);
+socket_set_option($sock, SOL_SOCKET, SO_BROADCAST, 1);
+$buf = "<nmea_rmc>$"."GPRMC,123519,A,4807.038,N,01131.000,E,022w.4,084.4,230394,003.1,W*6A </nmea_rmc>";
+socket_write($sock,$buf,strlen($buf));
+socket_close($sock);
 
-//Cette option permet d'éviter la mise en cache du WSDL, qui se renouvelle toutes les 24 heures... Pour le développement, ce n'est pas génial !!!
-ini_set('soap.wsdl_cache_enabled', 0);
+    $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
 
-//On doit passer le fichier WSDL du Service en paramètre de l'objet SoapClient
-$service=new SoapClient("http://127.0.0.1/PFE-Simulateur/Test_SOAP/GeolocalizationReferenceBasic.wsdl");
-
-//On accède à la méthode de notre classe DateServeur, déclaré dans notre SoapServer
-$taballservices = $service->retourDate();
-
-echo $service->nmea_rmc();
-
-
-//On renvoie le résutat de notre méthode, pour voir...
-print_r($taballservices);
+}
+Catch(Exception $ex)
+{
+    echo $ex;
+}
+echo "Broadcast";
+?>
--- Test_SOAP/SoapServeur.php
+++ Test_SOAP/SoapServeur.php
@@ -5,19 +5,14 @@
  *
  */
 
-//Cette classe pourra contenir d'autres méthodes accessibles via le SoapServer
-class DateServer{
 
-    //On déclare notre méthode qui renverra la date et la signature du serveur dans un tableau associatif...
-    function retourDate(){
+ini_set('soap.wsdl_cache_enabled', 0);
 
-        $tab = array(
-            'serveur' => $_SERVER['SERVER_SIGNATURE'],
-            'date' => date("d/m/Y")
-        );
 
-        return $tab;
-    }
+$serversoap=new SoapServer(Null,array('uri'=> '192.168.10.1/nmea_rmc'));
+
+
+
 
     function nmea_rmc()
     {
@@ -25,22 +20,13 @@ class DateServer{
            return "<nmea_rmc>$"."GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A </nmea_rmc>";
 
     }
-}
 
-//Cette option du fichier php.ini permet de ne pas stocker en cache le fichier WSDL, afin de pouvoir faire nos tests
-//Car le cache se renouvelle toutes les 24 heures, ce qui n'est pas idéal pour le développement
-ini_set('soap.wsdl_cache_enabled', 0);
 
-//L'instanciation du SoapServer se déroule de la même manière que pour le client : voir la doc pour plus d'informations sur les
-//Différentes options disponibles
-$serversoap=new SoapServer("http://127.0.0.1/PFE-Simulateur/Test_SOAP/GeolocalizationReferenceBasic.wsdl");
 
-//Ici nous déclarons la classe qui sera servie par le Serveur SOAP, c'est cette déclaration qui fera le coeur de notre Servie WEB.
-//Je déclare que je sers une classe contenant des méthodes accessibles, on peut aussi déclarer plus simplement des fonctions
-//par l'instruction addFunction() : $serversoap->addFunction("retourDate"); à ce moment-là nous ne faisons pas de classe.
+$serversoap->addFunction('nmea_rmc');
 
-//Noter le style employé pour la déclaration : le nom de la classe est passé en argument de type String, et non pas de variable...
-$serversoap->setClass("DateServer");
+
+
 
 
 //Ici, on dit très simplement que maintenant c'est à PHP de prendre la main pour servir le Service WEB : il s'occupera de l'encodage XML, des
