f92d7916ed19926e131f4aa750b3536bf82785cd
ndaussy <ndaussy@gmail.com>
2014-01-20 03:45:28 +0100

    Fonctionnement du kml & accés au donnée, restant insertion en BDD

--- application/models/kml_model.php
+++ application/models/kml_model.php
@@ -16,28 +16,20 @@ class Kml_model extends CI_Model {
 
     public function save_kml($filename,$simulation)
     {
-        //$filename='https://github.com/ndaussy/PFE-Simulateur/blob/master/kml/TCAR.kml';
+
         $content = utf8_encode(file_get_contents($filename));
         $kml = simplexml_load_string($content);
-        var_dump($kml->StyleMap);
-        var_dump($kml->Folder[0]);
-        echo "Value ".(string)$kml->Folder[0];
-        //var_dump($kml->Folder->Placemark->name);
-       // var_dump($kml->Folder->Placemark->Point->coordinate);
-        //var_dump($kml->folder->Placemark);
-        /*
-         * Placemark>
-   <name>Boulingrin</name>
-   <Point>
-         *
-         */
 
-      /*  for($a=0;$a<count($kml->Placemark);$a++)
+
+        for($a=0;$a<$kml->Document->Folder->Placemark->count();$a++)
         {
-            var_dump($kml->Placemark[$a]->name);
-
+            echo "Value ".(string)$kml->Document->Folder->Placemark[$a]->name;
+            echo "Value ".(string)$kml->Document->Folder->Placemark[$a]->Point->coordinates;
         }
-        */
+
+       
+
+
 
         return true;
 
