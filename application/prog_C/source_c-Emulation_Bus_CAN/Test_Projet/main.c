71973f77ccda7d44829783500f6fd3e33c76b3d9
ndaussy <ndaussy@gmail.com>
2014-01-21 13:44:09 +0100

    ajout des kml + de la source C

--- application/prog_C/source_c-Emulation_Bus_CAN/Test_Projet/main.c
+++ application/prog_C/source_c-Emulation_Bus_CAN/Test_Projet/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <winsock2.h>
 #include "vs_can_api.h"
 
 
 
 int main(int argc, char* argv[])
 {
-int i;
 VSCAN_STATUS status;
 VSCAN_HANDLE handle;
 VSCAN_MSG msgs[8];
@@ -15,12 +15,37 @@ DWORD written;
 msgs[0].Flags = VSCAN_FLAGS_EXTENDED;
 msgs[0].Size = 8;
 char* byte[8];
+int i;
 
-for (i=1;i<argc;i++)
+WSADATA WSAData;
+SOCKET sock;
+SOCKADDR_IN sin;
+WSAStartup(MAKEWORD(2,0), &WSAData);
+char buffer[100];
+char* ok="READY";
+int port;
+
+port=(int)strtol(argv[argc-1], NULL, 16);
+sock = socket(AF_INET,SOCK_STREAM,0);
+sin.sin_addr.s_addr	= inet_addr("127.0.0.1");
+sin.sin_family		= AF_INET;
+sin.sin_port		= htons(port);
+
+bind(sock, (SOCKADDR *)&sin, sizeof(sin));
+
+connect(sock, (SOCKADDR *)&sin, sizeof(sin));
+send(sock, "READY", sizeof("READY"), 0);
+
+do
+{
+recv(sock, buffer, sizeof(buffer), 0);
+}while(strcmp(buffer, ok)!=1);
+
+for (i=1;i<argc-1;i++)
 {
     byte[i-1] = argv[i];
 }
-msgs[0].Id= (UINT32)strtol(argv[argc-1], NULL, 16);
+msgs[0].Id= (UINT32)strtol(argv[argc-2], NULL, 16);
 
 
 for(i=0;i<8;i++)
@@ -34,40 +59,49 @@ printf("%x\n", msgs[0].Size);
 
 for(i=0;i<20240;i++)
 {
-   status = VSCAN_Write(handle, msgs, 1, &written);
+    status = VSCAN_Write(handle, msgs, 1, &written);
 }
 
 for(i=0;i<8;i++)
 {
-printf("%x\n",msgs[0].Data[i]);
+    printf("%x\n",msgs[0].Data[i]);
 }
 
 switch(status)
 {
     case VSCAN_ERR_OK : printf("Everything is okay\n");
-    break;
+                        break;
+
     case VSCAN_ERR_ERR : printf("General error\n");
-    break;
+                         break;
+
     case VSCAN_ERR_NO_DEVICE_FOUND : printf("No CAN device was found with specific function\n");
-    break;
+                                     break;
+
     case VSCAN_ERR_SUBAPI : printf("Error in subordinated library\n");
-    break;
+                            break;
+
     case VSCAN_ERR_NOT_ENOUGH_MEMORY : printf("Not enough memory\n");
-    break;
+                                       break;
+
     case VSCAN_ERR_NO_ELEMENT_FOUND : printf("No request element available\n");
-    break;
+                                      break;
+
     case VSCAN_ERR_INVALID_HANDLE : printf("Invalid handle\n");
-    break;
+                                    break;
+
     case VSCAN_ERR_IOCTL : printf("Ioctl request failed\n");
-    break;
+                           break;
+
     case VSCAN_ERR_MUTEX : printf("Problem with a used mutex in CAN API\n");
-    break;
+                           break;
+
     case VSCAN_ERR_CMD : printf("Problem to complete a given command\n");
-    break;
+                         break;
 }
 
 status = VSCAN_Close(handle);
-
+WSACleanup();
 return 0;
 }
 
